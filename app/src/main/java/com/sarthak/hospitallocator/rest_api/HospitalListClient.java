328c9ae4d49bbdeafee4f00b89f030b489e23d62
Sarthak Aggarwal <sarthakaggarwal97@gmail.com>
2018-06-10 16:07:51 +0530

    first commit

---
+++ app/src/main/java/com/sarthak/hospitallocator/rest_api/HospitalListClient.java
@@ -0,0 +1,28 @@
+package com.sarthak.hospitallocator.rest_api;
+
+import com.sarthak.hospitallocator.models.DetailResult;
+import com.sarthak.hospitallocator.models.DistanceResult;
+import com.sarthak.hospitallocator.models.PlaceList;
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+
+/**
+ * Created by Mayank Gupta on 11-12-2017.
+ */
+
+public interface HospitalListClient {
+
+    @GET("place/nearbysearch/json")
+    Call<PlaceList> getNearbyHospitals (@QueryMap Map<String,String> params);
+
+    @GET("distancematrix/json")
+    Call<DistanceResult> getHospitalDistances(@QueryMap Map<String,String> params);
+
+    @GET("place/details/json")
+    Call<DetailResult> getHospitalDetails(@QueryMap Map<String ,String> params);
+}
